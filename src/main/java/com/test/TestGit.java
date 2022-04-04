e567c17a934e6ffeca5939b865890e944ec80233
DearSil <2437495924@qq.com>
2022-04-04 21:35:59 +0800

    Idea Second Commit!

--- src/main/java/com/test/TestGit.java
+++ src/main/java/com/test/TestGit.java
@@ -3,5 +3,6 @@ package com.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("First Test Git ");
+        System.out.println("Second Test Git");
     }
 }
